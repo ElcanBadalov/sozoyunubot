8b5c04a5f51923fb1d8787f85f51c8f6f6fadd68
Sekret009 <87939124+Sekret009@users.noreply.github.com>
2021-07-25 14:48:32 +0000

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 TOKEN = "1728319045:AAHvBP_WygeCtzwKoXDH7u5H35YcyTulwJA"
-with open('sozler.txt', 'r', encoding='utf-8') as file:
+with open('sozler.txt', 'r', encoding='utf-8') as file: #oyun ucun sozleri sozler.txt faylindan goturecekturecek
     word_list = file.read().splitlines()
